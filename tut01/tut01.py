56bb96e9294477932e7a84eeba4bc2860f6efe88
NimbleNitesh <nitesh620137@gmail.com>
2022-08-27 18:51:57 +0530

    Tut 1 completed

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,9 @@
-def factorial(int x):
+def factorial(x):
+    fact=1
+    for i in range(1,x+1):
+        fact=fact*i
+    print(fact)
     
 
-x=int(input("Enter the number whose factorial is to be found"))
+x=int(input("Enter the number whose factorial is to be found \n"))
 factorial(x)
