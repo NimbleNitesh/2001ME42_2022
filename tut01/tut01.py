5ad22ea428111a0dc282162dc55a74c8e07c1634
NimbleNitesh <nitesh620137@gmail.com>
2022-09-14 19:23:12 +0530

    Update tut01.py
    Added version if-else statement

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -147,6 +147,13 @@ def octact_identification(mod=5000):
     #print(df.head(5))
     df.to_csv(r"octant_output.csv", encoding='utf-8', index=False)
 
+from platform import python_version
+ver = python_version()
+
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
 
 mod=5000
