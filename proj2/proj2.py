75b797b91524c6b748de18e3f23072f355f921b0
NimbleNitesh <nitesh620137@gmail.com>
2022-11-30 03:03:32 +0530

    Added code of project 2

--- proj2/proj2.py
+++ proj2/proj2.py
@@ -1,30 +1,90 @@
-
+import streamlit as st
 from datetime import datetime
-start_time = datetime.now()
-
-#Help
-def proj_octant_gui():
-	pass
-
-
-###Code
-
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-	print("Correct Version Installed")
-else:
-	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+from streamlit_option_menu import option_menu
+from subprocess import call
+import pandas as pd
+import os
+import sys
+os.system('cls')
 
 
-proj_octant_gui()
+with st.sidebar:
+    selected=option_menu(
+        menu_title=None,
+        options=["Home", "Bulk Convert", "Contact","About"],
+        icons=["house","collection-fill","envelope","book"],
+        menu_icon="cast", default_index=0,
+        styles={
+        "container": {"padding": "0!important", "background-color": "#495057"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "green"},
+    }
+)
 
 
+if selected=="Home":
+    st.subheader("Hi, Let's begin with creating ultimate Web based Interface")
+    st.title("Python: Project 2")
+    uploaded_files = st.file_uploader("Upload excel files", accept_multiple_files=True)
+    st.caption('''Make sure that current working directory doesn't has a folder named "input". If present kindly rename it.''')
+    form=st.form("mod", clear_on_submit=False)
+    mod=form.text_input("mod")
+    submit=form.form_submit_button()
+    my_bar = st.progress(0)
+    if submit:
+        current_directory = os.getcwd()
+        final_directory = os.path.join(current_directory, r'input')
+        if not os.path.exists(final_directory):
+            os.makedirs(final_directory)
+        final_directory = os.path.join(current_directory, r'output')
+        if not os.path.exists(final_directory):
+            os.makedirs(final_directory)
+        for file in uploaded_files:
+            print(file.name)
+            filename=file.name
+            try:
+                df=pd.read_excel(file)
+                df.to_excel('./input/%s'%filename,index=False)
+            except Exception as e:
+                print('Error: ' + str(e))
+        # print(mod)
+        src=".\input"
+        call(["python", "tut07.py", str(mod), src])
+        my_bar.progress(100)
+        st.success('This is a success message!', icon="✅")
 
 
+elif selected=="Bulk Convert":
+    st.subheader("Hi, Let's begin with creating ultimate Web based Interface")
+    st.title("Python: Project 2")
+    form=st.form("Input the folder path:", clear_on_submit=False)
+    src=form.text_input("Input the folder path:")
+    st.caption('''For Example: "C:\\Users\\Dell Vostro 3491\\OneDrive\\Documents\\fluid".[without double quotes]''')
+    submit=form.form_submit_button()
+    form=st.form("mod:", clear_on_submit=False)       
+    mod=form.text_input("mod")
+    submit=form.form_submit_button()
+    my_bar = st.progress(0)
+    if(submit):
+        current_directory = os.getcwd()
+        final_directory = os.path.join(current_directory, r'output')
+        if not os.path.exists(final_directory):
+            os.makedirs(final_directory)
+        call(["python", "tut07.py", str(mod), src])
+        my_bar.progress(100)
+        st.success('This is a success message!', icon="✅")
 
 
-#This shall be the last lines of the code.
-end_time = datetime.now()
-print('Duration of Program Execution: {}'.format(end_time - start_time))
+elif selected=="Contact":
+    st.subheader('Deepanshu Chaudhary:')
+    st.write('deepanshu_2001me21@iitp.ac.in')
+    st.subheader('Nitesh Srivastava:')
+    st.write('nitesh_2001me42@iitp.ac.in')
+    
+
+elif selected=="About":
+    st.title('About Project')
+    # st.text("This is our project 2, developed by Nitesh Srivastava[2001ME42] and Deepanshu Chaudhary[2001ME21]. We have two types of conversions: Normal and Bulk Convert. Normal Convert is in Home Tab. Here the user can directly upload .xlsx files and value of mod and then get result. Bulk Convert is a bit different. Here the User has to manually input the path of the folder which contains all .xlsx files alongwith mod value. All the output is stored in an output folder which is located(in not present then created) in the current working directory.")
+    loooong_text = ' '.join(["This is our project 2, developed by Nitesh Srivastava[2001ME42] and Deepanshu Chaudhary[2001ME21].\nWe have two types of conversions: Normal and Bulk Convert. Normal Convert is in Home Tab. Here the user can directly upload .xlsx files and value of mod and then get result. Bulk Convert is a bit different. Here the User has to manually input the path of the folder which contains all .xlsx files alongwith mod value. All the output is stored in an output folder which is located(in not present then created) in the current working directory."])
+    st.markdown(loooong_text)
---
+++ proj2/tut07.py
@@ -0,0 +1,492 @@
+import pandas as pd
+import numpy as np
+import glob
+import openpyxl
+from openpyxl.styles import Font
+from openpyxl import load_workbook
+from openpyxl import Workbook
+from openpyxl.styles import Border, Side
+from datetime import datetime
+import sys
+from subprocess import call
+
+try:
+	def set_border(cell_range,ws):
+	    thin = Side(border_style="medium",color="000000")
+	    for row in ws[cell_range]:
+	        for cell in row:
+	            cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
+except:
+	print('Error in set_border function')
+	exit()
+try:            
+	# function for highlighting cells
+	def set_highlight(r,c,ws):
+	    # print(r,c)
+	#     wb.active
+	    cell=ws.cell(row = r, column = c)
+	    my_high = openpyxl.styles.colors.Color(rgb='00FFFF00')
+	    my_fill = openpyxl.styles.fills.PatternFill(patternType='solid', fgColor=my_high)
+	    cell.fill = my_fill
+except:
+	print('Error in set_highlight function')
+	exit()
+
+try:
+	def OctantCount(l, r, Octant):
+	    thisdict={'+1':0, '-1':0, '+2':0, '-2':0, '+3':0, '-3':0, '+4':0, '-4':0}
+	    for i in range(l, r+1):
+	#         print(i)
+	        thisdict[Octant[i]]+=1
+	    return thisdict
+except:
+	print('Error in OctantCount function')
+	exit()
+
+try:
+	def GiveRank(thisdict):
+	    curdict={}
+	    l1=['Rank of +1', 'Rank of -1', 'Rank of +2', 'Rank of -2', 'Rank of +3', 'Rank of -3', 'Rank of +4', 'Rank of -4']
+	    l2=list(thisdict.values())
+	    pairlist=list(zip(l1, l2))
+	    pairlist.sort(key=lambda x: x[1])
+	    pairlist.reverse()
+	    i=0
+	    while i<8:
+	        curval=pairlist[i][1]
+	        curdict[pairlist[i][0]]=str(i+1)
+	        i+=1
+	    return curdict
+except:
+	print('Error in GiveRank function')
+	exit()
+
+try:
+	def TransitionCount(l, r, Octant):
+	    thisdict={' +1': 0, ' -1': 0, ' +2': 0, ' -2': 0, ' +3': 0, ' -3': 0, ' +4': 0, ' -4': 0}
+	    finaldict={' +1':thisdict, ' -1':thisdict, ' +2':thisdict, ' -2':thisdict, ' +3':thisdict, ' -3':thisdict, ' +4':thisdict, ' -4':thisdict}
+	    #finaldict[i][j] means trans from i to j
+	    for i in range(l, r):
+	        _from=' '+Octant[i]
+	        _to=' '+Octant[i+1]
+	        finaldict[_from][_to]+=1
+	    if r+1<len(Octant):
+	        _from=' '+Octant[r]
+	        _to=' '+Octant[r+1]
+	        finaldict[_from][_to]+=1
+	    return finaldict
+except:
+	print('Error in TransitionCount function')
+	exit()
+
+#Help
+def octant_analysis(mod, src):
+	# print(type(mod))
+	st_border_=[]
+	mod=int(mod)
+	try:
+		# filenames = glob.glob(".\input\*.xlsx")
+		src=src+"\\*.xlsx"
+		filenames=glob.glob(src)
+	except:
+		print('Errorrrr in Reading file from input folder')
+		exit()
+	for file in filenames:
+		f_name=str(file)
+		f_name=f_name.replace(".xlsx","")
+		f_name=f_name[-3:]
+		j=len(f_name)-1
+		s_name=""
+		print("Reading file = ",file)
+		str_mod=str(mod)
+		stringg=f_name+"vel_octant_analysis_mod%s"%str_mod
+		stringg1=stringg
+		df=pd.read_excel(file)
+		# df=pd.read_excel(r'C:\Users\Dell Vostro 3491\OneDrive\Documents\GitHub\2001ME42_2022\tut07\input\5.3.xlsx')
+		# set max columns to none
+		# pd.set_option("display.max_columns", None)
+		# set colwidth hidher
+		# pd.set_option('display.max_colwidth', 100)
+		try:
+			df = df.reindex(columns = df.columns.tolist() + ['U Avg', 'V Avg', 'W Avg', "U'=U - U avg", "V'=V - V avg", "W'=W - W avg", 'Octant', ' ', '  ', 'Overall Octant Count', '+1', '-1', '+2', '-2', '+3', '-3', '+4', '-4', 'Rank of +1', 'Rank of -1', 'Rank of +2', 'Rank of -2', 'Rank of +3', 'Rank of -3', 'Rank of +4', 'Rank of -4', 'Octant ID of Rank 1', 'Octant name of Rank 1', '   ', '    ', 'Overall Transition Count', ' +1', ' -1', ' +2', ' -2', ' +3', ' -3', ' +4', ' -4', '     ', 'Longest Subsquence Length', '      ', '       ', '        ', 'Longest Subsquence Length with Range', '         ', '          '])
+		except:
+			print('Error in inserting new columns')
+			exit()
+		U_Avg=df['U'].mean()
+		# print(U_Avg)
+		U_Avg=round(U_Avg, 3)#setting precision to 8
+		V_Avg=df['V'].mean()
+		V_Avg=round(V_Avg, 3)
+		W_Avg=df['W'].mean()
+		W_Avg=round(W_Avg, 3)
+		U_Avg=str(U_Avg)
+		V_Avg=str(V_Avg)
+		W_Avg=str(W_Avg)
+		df.at[0, 'U Avg'] = U_Avg
+		df.at[0, 'V Avg'] = V_Avg
+		df.at[0, 'W Avg'] = W_Avg
+		U_Avg=float(U_Avg)
+		V_Avg=float(V_Avg)
+		W_Avg=float(W_Avg)
+		df["U'=U - U avg"]=df['U']-U_Avg
+		df["V'=V - V avg"]=df['V']-V_Avg
+		df["W'=W - W avg"]=df['W']-W_Avg
+		try:
+			df["U'=U - U avg"] = df["U'=U - U avg"].apply(lambda x: format(float(x),".3f"))
+			df["V'=V - V avg"] = df["V'=V - V avg"].apply(lambda x: format(float(x),".3f"))
+			df["W'=W - W avg"] = df["W'=W - W avg"].apply(lambda x: format(float(x),".3f"))
+		except:
+			print('Error in setting decimal precision')
+			exit()
+		U_list=df["U'=U - U avg"].tolist()
+		V_list=df["V'=V - V avg"].tolist()
+		W_list=df["W'=W - W avg"].tolist()
+		n=len(U_list)
+		for i in range(n):
+		    U_list_i=float(U_list[i])
+		    V_list_i=float(V_list[i])
+		    W_list_i=float(W_list[i])
+		    if U_list_i>=0.0 and V_list_i>=0.0:
+		        if W_list_i>0:
+		            df.at[i, 'Octant']="+1"
+		        else:
+		            df.at[i, 'Octant']="-1"
+		    elif U_list_i<0.0 and V_list_i>=0.0:
+		        if W_list_i>0:
+		            df.at[i, 'Octant']="+2"
+		        else:
+		            df.at[i, 'Octant']="-2"
+		    elif U_list_i<0.0 and V_list_i<0.0:
+		        if W_list_i>0:
+		            df.at[i, 'Octant']="+3"
+		        else:
+		            df.at[i, 'Octant']="-3"
+		    elif U_list_i>=0.0 and V_list_i<0.0:
+		        if W_list_i>0:
+		            df.at[i, 'Octant']="+4"
+		        else:
+		            df.at[i, 'Octant']="-4"
+		cur_index=1
+
+
+		try:
+			#Overall Octant Count and Rank
+			lx=[cur_index, 'N']
+			df.at[cur_index, 'Overall Octant Count']='Octant ID'
+			df.at[cur_index, '+1']=df.at[cur_index, ' +1']='+1'
+			df.at[cur_index, '-1']=df.at[cur_index, ' -1']='-1'
+			df.at[cur_index, '+2']=df.at[cur_index, ' +2']='+2'
+			df.at[cur_index, '-2']=df.at[cur_index, ' -2']='-2'
+			df.at[cur_index, '+3']=df.at[cur_index, ' +3']='+3'
+			df.at[cur_index, '-3']=df.at[cur_index, ' -3']='-3'
+			df.at[cur_index, '+4']=df.at[cur_index, ' +4']='+4'
+			df.at[cur_index, '-4']=df.at[cur_index, ' -4']='-4'
+			st_high=[]
+			Oct_list=df['Octant'].tolist()
+			thisdict=OctantCount(0, n-1, Oct_list)
+			rankdict=GiveRank(thisdict)
+			cur_index+=1
+			df.at[cur_index, 'Overall Octant Count']='Overall Count'
+			OctantName={
+			        '+1':'Internal outward interaction',
+			        '-1':'External outward interaction',
+			        '+2':'External Ejection',
+			        '-2':'Internal Ejection',
+			        '+3':'External inward interaction',
+			        '-3':'Internal inward interaction',
+			        '+4':'Internal sweep',
+			        '-4':'External sweep'
+			    }
+			countdict={
+			        '+1':0,
+			        '-1':0,
+			        '+2':0,
+			        '-2':0,
+			        '+3':0,
+			        '-3':0,
+			        '+4':0,
+			        '-4':0
+			    }
+			rank_1=''
+			cur_col=23
+			cnt=0
+			for i in thisdict:
+				cnt+=1
+				df.at[cur_index, i]=thisdict[i]
+				txt='Rank of {}'
+				txt=txt.format(i)
+				df.at[cur_index, txt]=rankdict[txt]
+				if rankdict[txt]=='1':
+					cur_col+=cnt
+					df.at[cur_index, 'Octant ID of Rank 1']=i
+					df.at[cur_index, 'Octant name of Rank 1']=OctantName[i]
+			st_high.append([cur_index, cur_col-1])
+			cur_index+=1
+			# print(thisdict)
+			x=mod
+			l=[]
+			cur=0
+			row_cnt=len(df.index)
+			while cur<=row_cnt:
+			    l.append(cur)
+			    cur+=x
+			l_n=len(l)
+			for i in range(l_n):
+			    low=l[i]
+			    high=low+x-1
+			    if high>=n:
+			        high=n-1
+			    thisdict=OctantCount(low, high, Oct_list)
+			    rankdict=GiveRank(thisdict)
+			    txt="{}-{}"
+			    txt=txt.format(low, high)
+			    df.at[cur_index, 'Overall Octant Count']=txt
+			    cur_col=23
+			    cnt=0
+			    for i in thisdict:
+			        cnt+=1
+			        df.at[cur_index, i]=thisdict[i]
+			        txt='Rank of {}'
+			        txt=txt.format(i)
+			        df.at[cur_index, txt]=rankdict[txt]
+			        if rankdict[txt]=='1':
+			#             print(txt, i)
+			            df.at[cur_index, 'Octant ID of Rank 1']=i
+			            df.at[cur_index, 'Octant name of Rank 1']=OctantName[i]
+			            countdict[i]+=1
+			            cur_col+=cnt
+			    st_high.append([cur_index, cur_col-1])
+			    cur_index+=1
+			ly=[cur_index, 'AF']
+			st_border_.append([lx, ly])
+			cur_index+=3
+			lx=[cur_index, 'AC']
+			df.at[cur_index, 'Rank of +4']='Octant ID'
+			df.at[cur_index, 'Rank of -4']='Octant Name'
+			df.at[cur_index, 'Octant ID of Rank 1']='Count of Rank 1 Mod Values'
+			cur_index+=1
+			for i in countdict:
+			    df.at[cur_index, 'Rank of +4']=i
+			    df.at[cur_index, 'Rank of -4']=OctantName[i]
+			    df.at[cur_index, 'Octant ID of Rank 1']=countdict[i]
+			    cur_index+=1
+			ly=[cur_index, 'AF']
+			st_border_.append([lx, ly])
+			#     print(low, high)
+			#     print(thisdict)
+		except:
+			print('Error in Overall Octant Count and Rank dataframe')
+			exit()
+
+
+		try:
+			#Transitions
+			#'    ', 'Overall Transition Count', ' +1'
+			transitiondict=TransitionCount(0, n-1, Oct_list)
+			lx=[1, 'AI']
+			df.at[1, 'Overall Transition Count']='Octant #'
+			df.at[0, ' +1']='To'
+			df.at[2, '    ']='From'
+			# print(transitiondict)
+			cur_index=2
+			for i in transitiondict:
+			    df.at[cur_index, 'Overall Transition Count']=i[1:];
+			    for j in transitiondict[i]:
+			        df.at[cur_index, j]=transitiondict[i][j]
+			    cur_index+=1
+			ly=[cur_index, 'AQ']
+			cur_index+=1
+			for k in range(l_n):
+			    cur_index+=2
+			    df.at[cur_index, 'Overall Transition Count']='Mod Transition Count'
+			    cur_index+=1
+			    low=l[k]
+			    high=low+x-1
+			    if high>=n:
+			        high=n-1
+			    txt="{}-{}"
+			    txt=txt.format(low, high)
+			    df.at[cur_index, 'Overall Transition Count']=txt
+			    cur_index+=1
+			    lx=[cur_index, 'AI']
+			    df.at[cur_index, 'Overall Transition Count']='Octant #'
+			    df.at[cur_index-1, ' +1']='To'
+			    df.at[cur_index+1, '    ']='From'
+			    transitiondict=TransitionCount(low, high, Oct_list)
+			    for i in transitiondict:
+			        df.at[cur_index, i]=i[1:];
+			    cur_index+=1
+			    for i in transitiondict:
+			        df.at[cur_index, 'Overall Transition Count']=i[1:];
+			        for j in transitiondict[i]:
+			            df.at[cur_index, j]=transitiondict[i][j]
+			        cur_index+=1
+			    ly=[cur_index, 'AQ']
+			    st_border_.append([lx, ly])
+		except:
+			print('Error in setting transition dataframe')
+		        
+		        
+		        
+		try:
+			#Longest Subsequence Length        
+			#'Longest Subsquence Length', '      ', '       ', '        '
+			#'Longest Subsquence Length with Range', '         ', '          '        
+			    #creating a dictionary having key each octant ID and value list of size 2
+			    #having first value as Longest Subsequence Length and second value as 
+			    #frequency of Longest Subsequence Length
+			thisdict={
+			    '+1':[0, 0],
+			    '-1':[0, 0],
+			    '+2':[0, 0],
+			    '-2':[0, 0],
+			    '+3':[0, 0],
+			    '-3':[0, 0],
+			    '+4':[0, 0],
+			    '-4':[0, 0]
+			}
+			    #creating a dictionary having key each octant ID and value a list storing 
+			    #all the starting indices of Longest Subsequence Length
+			newdict={
+			    '+1':[],
+			    '-1':[],
+			    '+2':[],
+			    '-2':[],
+			    '+3':[],
+			    '-3':[],
+			    '+4':[],
+			    '-4':[]
+			}
+			i=0
+			while i<n:
+			    cur_val=Oct_list[i]
+			    cur_freq=1
+			    j=i+1
+			    while j<n :
+			        if Oct_list[j]!=cur_val:
+			            break
+			        cur_freq+=1
+			        j+=1
+			    if thisdict[cur_val][0]==cur_freq:
+			        thisdict[cur_val][1]+=1
+			        newdict[cur_val].append(i)
+			    elif thisdict[cur_val][0]<cur_freq:
+			        newdict[cur_val].clear()
+			        newdict[cur_val].append(i)
+			        thisdict[cur_val][1]=1
+			        thisdict[cur_val][0]=cur_freq
+			    i=j
+		except:
+			print('Error in Longest Subsequence Length dataframe')
+			exit()
+
+
+		try:
+			#'Longest Subsquence Length with Range', '         ', '          '  
+			cur_index=1
+			lx=[1, 'AW']
+			df.at[cur_index, 'Longest Subsquence Length']='Octant##'
+			df.at[cur_index, '      ']='Longest Subsquence Length'
+			df.at[cur_index, '       ']='Count'
+			df.at[cur_index, 'Longest Subsquence Length with Range']='Octant###'
+			df.at[cur_index, '         ']='Longest Subsquence Length'
+			df.at[cur_index, '          ']='Count'
+			cur_index+=1
+			for i in thisdict:
+			    df.at[cur_index, 'Longest Subsquence Length']=i
+			    df.at[cur_index, '      ']=thisdict[i][0]
+			    df.at[cur_index, '       ']=thisdict[i][1]
+			    cur_index+=1
+			cur_index=2
+			for i in newdict:
+			    #df['Count_2'][cur_index]=i
+			    df.at[cur_index, 'Longest Subsquence Length with Range']=i
+			#     df['Longest Subsequence Length_2'][cur_index]=thisdict[i][0]
+			    df.at[cur_index, '         ']=thisdict[i][0]
+			#     df['Count_3'][cur_index]=thisdict[i][1]
+			    df.at[cur_index, '          ']=thisdict[i][1]
+			    cur_index+=1
+			    df.at[cur_index, 'Longest Subsquence Length with Range']='Time'
+			    df.at[cur_index, '         ']='From'
+			    df.at[cur_index, '          ']='To'
+			    cur_index+=1
+			    for j in newdict[i]:
+			        df.at[cur_index, '         ']=df['T'][j]
+			        df.at[cur_index, '          ']=df['T'][j+thisdict[i][0]-1]
+			        cur_index+=1
+			ly=[cur_index, 'AY']
+			st_border_.append([lx, ly])
+			# df['id'] = df['id'].astype("string")     
+			# df.style.applymap(lambda x: "background-color: red" if x==1 else "background-color: white")
+			# df.head()
+		except:
+			print('Error in Longest Subsquence Length with Range dataframe')
+			exit()
+
+
+		try:
+			df.to_excel('./output/%s.xlsx'%stringg,index=False) #creating output excel file
+		except:
+			print('Error in exporting xlsx files')
+			print(stringg)
+			exit()
+
+
+		try:
+			writer = load_workbook(filename='./output/%s.xlsx'%stringg1)
+			worksheet = writer['Sheet1']
+			for j in range(len(st_high)):
+			    set_highlight(st_high[j][0]+2,st_high[j][1],worksheet)
+
+			#         workbook = xlsxwriter.Workbook('%s.xlsx'%stringg1)
+
+			# set_border('N3:AF7',worksheet)
+			set_border('AI3:AQ11',worksheet)
+			# set_border('AC9:AE17',worksheet)
+			# set_border('AI17:AQ25',worksheet)
+			set_border('AS3:AU11',worksheet)
+			# set_border('N3:AF7',worksheet)
+			# set_border('AI31:AQ39',worksheet)
+			# set_border('AI46:AQ53',worksheet)
+			# set_border('AI59:AQ67',worksheet)
+			# set_border('AW3:AY28',worksheet)
+			st_border_size=len(st_border_)
+			for i in range(st_border_size):
+				lx1=st_border_[i][0][0]
+				ly1=st_border_[i][0][1]
+				lx2=st_border_[i][1][0]
+				ly2=st_border_[i][1][1]
+				lx1+=2
+				lx2+=1
+				qq=ly1+str(lx1)+':'+ly2+str(lx2)
+				# print(qq)
+				set_border(qq, worksheet)
+			writer.save('./output/%s.xlsx'%stringg1)
+	##Read all the excel files in a batch format from the input/ folder. Only xlsx to be allowed
+	##Save all the excel files in a the output/ folder. Only xlsx to be allowed
+	## output filename = input_filename[_octant_analysis_mod_5000].xlsx , ie, append _octant_analysis_mod_5000 to the original filename. 
+		except:
+			print('Error in using openpyxl library')
+			exit()
+###Code
+def main(mod, src): 
+	start_time = datetime.now()
+	from platform import python_version
+	ver = python_version()
+
+	if ver == "3.8.10":
+		print("Correct Version Installed")
+	else:
+		print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+	octant_analysis(mod, src)
+	#This shall be the last lines of the code.
+	end_time = datetime.now()
+	print('Duration of Program Execution: {}'.format(end_time - start_time))
+
+
+if __name__ == "__main__":
+	mod=sys.argv[1]
+	src=sys.argv[2]
+	main(mod, src)
+
+
